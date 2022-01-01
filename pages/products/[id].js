ca2881e682714c739cde63ed1ac63da553ceae26
Stanoid <mujahidwadmohammad@gmail.com>
2022-01-01 11:14:51 +0300

    fixed build error

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -351,7 +351,7 @@ return null
                   <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
                   <div className="flex items-center space-x-3">
                     {productel.colors? productel.colors.map((color) => (
-                      <div  style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+                      <div key={color.color_name}  style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
                       <RadioGroup.Option
                         key={color.color_name}
                         
@@ -546,7 +546,7 @@ return {
         params:{id: String(product.id)}
     })),
    
-    fallback:true
+    fallback:false
 }
 
 }
