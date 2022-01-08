35b97eec10593615b43c17ae2eb29ac8ad67b248
Stanoid <mujahidwadmohammad@gmail.com>
2022-01-09 00:41:29 +0300

    eslint fix2

--- pages/uploadform/index.js
+++ pages/uploadform/index.js
@@ -204,7 +204,7 @@ let doo = new Promise(function(suc) {
           <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
             <div className="grid grid-cols-3 gap-6">
               <div className="col-span-3 sm:col-span-2">
-                <label for="company-website" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
                   Product name
                 </label>
                 <div className="mt-1 flex rounded-md shadow-sm">
@@ -215,7 +215,7 @@ let doo = new Promise(function(suc) {
             </div>
 
             <div>
-              <label for="about" className="block text-sm font-medium text-gray-700">
+              <label htmlFor="about" className="block text-sm font-medium text-gray-700">
                 Description
               </label>
               <div className="mt-1">
@@ -227,13 +227,13 @@ let doo = new Promise(function(suc) {
             </div>
 
             <div className="col-span-6 sm:col-span-3">
-                <label for="country" className="block text-sm font-medium text-gray-700">Catagory</label>
-                <select value={cat} onChange={(event)=>{setcat(event.target.value)}} id="country" name="country" autocomplete="country-name" className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <label htmlFor="country" className="block text-sm font-medium text-gray-700">Catagory</label>
+                <select value={cat} onChange={(event)=>{setcat(event.target.value)}} id="country" name="country" autoComplete="country-name" className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                
                  {
                       
                     cats&&cats.map((cat)=>{
-                     return <option value={cat.id} >{cat.attributes.Name}</option>
+                     return <option key={cat.id} value={cat.id} >{cat.attributes.Name}</option>
                      })
                  }
                 </select>
@@ -252,9 +252,9 @@ let doo = new Promise(function(suc) {
                 <div className="space-y-1 text-center">
                  
                   <div className="flex text-sm text-gray-600">
-                    <label for="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                    <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                     <svg className="mx-auto h-12 w-12 text-gray-400" stroke={MAIN_STYLE.primary} fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
                       <input  id="file-upload" onChange= {(e)=>{
                     //       const ims = {data};
@@ -314,18 +314,18 @@ let doo = new Promise(function(suc) {
               
 
             <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" className="block text-sm font-medium text-gray-700">Option name</label>
-                <input value={top}  onChange={(event)=>{settop(event.target.value)}} type="text" name="city" id="city" autocomplete="address-level2" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                <label htmlFor="city" className="block text-sm font-medium text-gray-700">Option name</label>
+                <input value={top}  onChange={(event)=>{settop(event.target.value)}} type="text" name="city" id="city" autoComplete="address-level2" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="region" className="block text-sm font-medium text-gray-700">Stock</label>
-                <input value={tstock}  onChange={(event)=>{settstock(event.target.value)}} type="number" name="region" id="region" autocomplete="address-level1" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                <label htmlFor="region" className="block text-sm font-medium text-gray-700">Stock</label>
+                <input value={tstock}  onChange={(event)=>{settstock(event.target.value)}} type="number" name="region" id="region" autoComplete="address-level1" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
 
               <div className="col-span-4 sm:col-span-3 lg:col-span-2">
-                <label for="postal-code" className="block text-sm font-medium text-gray-700">Price</label>
-                <input value={tprice}  onChange={(event)=>{settprice(event.target.value)}} type="number" name="postal-code" id="postal-code" autocomplete="postal-code" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">Price</label>
+                <input value={tprice}  onChange={(event)=>{settprice(event.target.value)}} type="number" name="postal-code" id="postal-code" autoComplete="postal-code" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
               <div className='col-span-12'>
               <div style={{float:'right'}} className="col-span-4 sm:col-span-3 lg:col-span-2">
@@ -339,17 +339,17 @@ let doo = new Promise(function(suc) {
 
         
               <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" className="block text-sm font-medium text-gray">Option name</label>
+                <label htmlFor="city" className="block text-sm font-medium text-gray">Option name</label>
               
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="region" className="block text-sm font-medium text-gray">Stock</label>
+                <label htmlFor="region" className="block text-sm font-medium text-gray">Stock</label>
                
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="postal-code" className="block text-sm font-medium text-gray">Price</label>
+                <label htmlFor="postal-code" className="block text-sm font-medium text-gray">Price</label>
               
               </div>
                {stock.map(stock=>{
@@ -357,17 +357,17 @@ let doo = new Promise(function(suc) {
                      <>
                      <div className='col-span-12' style={{borderBottom:'1px solid grey'}}></div>
                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" className="block text-sm font-medium text-black">{stock.ob}</label>
+                <label htmlFor="city" className="block text-sm font-medium text-black">{stock.ob}</label>
               
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="region" className="block text-sm font-medium text-black">{stock.stock}</label>
+                <label htmlFor="region" className="block text-sm font-medium text-black">{stock.stock}</label>
                
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="postal-code" className="block text-sm font-medium text-black">{stock.price}</label>
+                <label htmlFor="postal-code" className="block text-sm font-medium text-black">{stock.price}</label>
                 </div>
                      </>  
                      
@@ -396,13 +396,13 @@ let doo = new Promise(function(suc) {
               
 
             <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" className="block text-sm font-medium text-gray-700">Color name</label>
-                <input value={colorname}  onChange={(event)=>{setcolorname(event.target.value)}} type="text" name="city" id="city" autocomplete="address-level2" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                <label htmlFor="city" className="block text-sm font-medium text-gray-700">Color name</label>
+                <input value={colorname}  onChange={(event)=>{setcolorname(event.target.value)}} type="text" name="city" id="city" autoComplete="address-level2" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="region" className="block text-sm font-medium text-gray-700">Color</label>
-                <input value={colovalue}  onChange={(event)=>{setcolovalue(event.target.value)}} type="color" name="region" id="region" autocomplete="address-level1" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                <label htmlFor="region" className="block text-sm font-medium text-gray-700">Color</label>
+                <input value={colovalue}  onChange={(event)=>{setcolovalue(event.target.value)}} type="color" name="region" id="region" autoComplete="address-level1" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
               </div>
 
             
@@ -418,12 +418,12 @@ let doo = new Promise(function(suc) {
 
         
               <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" className="block text-sm font-medium text-gray">Color name</label>
+                <label htmlFor="city" className="block text-sm font-medium text-gray">Color name</label>
               
               </div>
 
               <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="region" className="block text-sm font-medium text-gray">Color</label>
+                <label htmlFor="region" className="block text-sm font-medium text-gray">Color</label>
                
               </div>
 
@@ -433,7 +433,7 @@ let doo = new Promise(function(suc) {
                      <>
                      <div className='col-span-12' style={{borderBottom:'1px solid grey'}}></div>
                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" className="block text-sm font-medium text-black">{color.color_name}</label>
+                <label htmlFor="city" className="block text-sm font-medium text-black">{color.color_name}</label>
               
               </div>
 
@@ -505,7 +505,7 @@ let doo = new Promise(function(suc) {
                     <input id="comments" name="comments" type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"/>
                   </div>
                   <div className="ml-3 text-sm">
-                    <label for="comments" className="font-medium text-gray-700">Comments</label>
+                    <label htmlFor="comments" className="font-medium text-gray-700">Comments</label>
                     <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
                   </div>
                 </div>
@@ -514,7 +514,7 @@ let doo = new Promise(function(suc) {
                     <input id="candidates" name="candidates" type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"/>
                   </div>
                   <div className="ml-3 text-sm">
-                    <label for="candidates" className="font-medium text-gray-700">Candidates</label>
+                    <label htmlFor="candidates" className="font-medium text-gray-700">Candidates</label>
                     <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
                   </div>
                 </div>
@@ -523,7 +523,7 @@ let doo = new Promise(function(suc) {
                     <input id="offers" name="offers" type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"/>
                   </div>
                   <div className="ml-3 text-sm">
-                    <label for="offers" className="font-medium text-gray-700">Offers</label>
+                    <label htmlFor="offers" className="font-medium text-gray-700">Offers</label>
                     <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
                   </div>
                 </div>
@@ -537,19 +537,19 @@ let doo = new Promise(function(suc) {
               <div className="mt-4 space-y-4">
                 <div className="flex items-center">
                   <input id="push-everything" name="push-notifications" type="radio" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"/>
-                  <label for="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
+                  <label htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
                     Everything
                   </label>
                 </div>
                 <div className="flex items-center">
                   <input id="push-email" name="push-notifications" type="radio" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"/>
-                  <label for="push-email" className="ml-3 block text-sm font-medium text-gray-700">
+                  <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
                     Same as email
                   </label>
                 </div>
                 <div className="flex items-center">
                   <input id="push-nothing" name="push-notifications" type="radio" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"/>
-                  <label for="push-nothing" className="ml-3 block text-sm font-medium text-gray-700">
+                  <label htmlFor="push-nothing" className="ml-3 block text-sm font-medium text-gray-700">
                     No push notifications
                   </label>
                 </div>
@@ -562,7 +562,7 @@ let doo = new Promise(function(suc) {
             </button>
           </div>
         </div>
-      </form>
+      </form>`
     </div>
   </div>
 </div> */}
@@ -571,7 +571,7 @@ let doo = new Promise(function(suc) {
 
 </div>
                <div style={{display:"flex",marginTop:20}}> 
-               <span   style={{margin:'auto'}} onClick={uploadImage} style={{backgroundColor:MAIN_STYLE.primary,color:"white",padding:"10px 15px",borderRadius:5,cursor:'pointer'}}>Upload image</span>
+               <span   onClick={uploadImage} style={{backgroundColor:MAIN_STYLE.primary,color:"white",padding:"10px 15px",borderRadius:5,cursor:'pointer',margin:"auto"}}>Upload image</span>
                </div>
                </div>
            </div>
