2e854e9f9b16f8933f8c73af2917db91b3b7ecd8
Stanoid <mujahidwadmohammad@gmail.com>
2022-05-05 03:10:35 +0300

    GRAPHICS!!!!!!!!!!!

--- comps/hero.js
+++ comps/hero.js
@@ -8,10 +8,10 @@ export default function Hero(props) {
        <div className='p-1 lg:p-6 xl:p-6 md:p-6 '>
          <div className='grid grid-cols-6 gap-x-1.5 gap-y-1.5' >
 
-      <div className='col-span-6 ' style={{height:'15vh',backgroundColor:'#285943'}}>c</div>
+   
 
            <div style={{width:'100%',position:"relative",backgroundColor:"red",height:"40vh"}} className='lg:col-span-4 xl:col-span-4 md:col-span-3 col-span-6'>
-           <Image src={'/bhd.jpg'} layout={"fill"}  />
+           <Image src={'/ltd2.jpg'}layout="fill" objectFit="cover" />
  {/* <SimpleImageSlider
         width={"100%"}
         height={"50%"}
@@ -28,7 +28,10 @@ export default function Hero(props) {
       /> */}
     
            </div>
-           <div style={{backgroundColor:"#2B2C28",height:"40vh"}} className='lg:col-span-2 xl:col-span-2 md:col-span-3 col-span-6'>b</div>
+           <div style={{backgroundColor:"#2B2C28",height:"40vh",position:"relative"}} className='lg:col-span-2 xl:col-span-2 md:col-span-3 col-span-6'>
+           <Image src={'/blk2.jpg'}layout="fill" objectFit="cover" />
+           </div>
+           
          </div>
        </div>
     )
